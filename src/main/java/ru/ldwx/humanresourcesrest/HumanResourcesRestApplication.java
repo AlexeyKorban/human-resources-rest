39575aeb41c305a0f4876b8e588f92db2151288e
Alexey Korban <ldwx@mail.ru>
2020-08-08 16:04:19 +0500

    Fix servlet initializer

--- src/main/java/ru/ldwx/humanresourcesrest/HumanResourcesRestApplication.java
+++ src/main/java/ru/ldwx/humanresourcesrest/HumanResourcesRestApplication.java
@@ -2,12 +2,18 @@ package ru.ldwx.humanresourcesrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HumanResourcesRestApplication {
+public class HumanResourcesRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourcesRestApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HumanResourcesRestApplication.class);
+	}
 }
